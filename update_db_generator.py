726bbab38f61e620e54cf1a6f162f425e37deb2a
bleach86 <14421739+bleach86@users.noreply.github.com>
2026-01-07 14:55:55 -0600

    copyright notice

--- update_db_generator.py
+++ update_db_generator.py
@@ -1,4 +1,22 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python
+
+# Copyright (c) 2025-2026 bleach86
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# You can download the latest version of this tool from:
+# https://github.com/bleach86/mister_cloud_saves
 
 import os
 import sys
